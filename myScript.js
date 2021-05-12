09a18f8a0c43ad0d135c0a8cee269738a470832c
dragsec <draganyes@gmail.com>
2021-05-12 17:26:42 +0200

    temporaneo

---
+++ myScript.js
@@ -0,0 +1,19 @@
+
+const app = new Vue({
+
+    el: '#MainAlbum',
+    data: {
+        AlbumsList: [],
+    },
+
+    methods: {
+        StampaAlbum: function(){
+                axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then((response1)=>{
+                this.AlbumsList.response1.data.response
+            });
+            
+
+        }
+    }
+})
